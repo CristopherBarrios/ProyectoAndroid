e03e70d3dc1d6d2f67a904b45d01fb15e1fcce2d
Cristopher Barrios <cjrbarrios@gmail.com>
2019-05-08 10:13:24 -0600

    Conectando a la base de datos

---
+++ HeroApi/includes/DbConnect.php
@@ -0,0 +1,23 @@
+<?php
+class DbConnect
+{
+    private $con;
+
+    function __construct()
+    {
+ 
+    }
+
+    function connect()
+    {
+        include_once dirname(__FILE__) . '/Constants.php';
+ 
+        $this->con = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        if (mysqli_connect_errno()) {
+            echo "Fallo en conectar a mysql: " . mysqli_connect_error();
+            return null;
+        }
+        return $this->con;
+    }
+ 
+}
---
+++ HeroApi/v1/index.php
@@ -0,0 +1,49 @@
+<?php 
+	require_once '../includes/DbOperation.php';
+	
+	$response = array(); 
+	
+	if(isset($_GET['op'])){
+		
+		switch($_GET['op']){
+			
+			case 'addheroes':
+				if(isset($_POST['name']) && isset($_POST['role'])){
+					$db = new DbOperation(); 
+					if($db->createHeroes($_POST['name'], $_POST['role'])){
+						$response['error'] = false;
+						$response['message'] = 'SE ha añadido la cuenta exitosamente';
+					}else{
+						$response['error'] = true;
+						$response['message'] = 'No se ha logrado ingresar la cuenta';
+					}
+				}else{
+					$response['error'] = true; 
+					$response['message'] = 'Required Parameters are missing';
+				}
+			break; 
+
+			case 'getheroes':
+				$db = new DbOperation();
+				$hero = $db->getHeroes();
+				if(count($hero)<=0){
+					$response['error'] = true; 
+					$response['message'] = 'No se ha encontrado la base de datos';
+				}else{
+					$response['error'] = false; 
+					$response['hero'] = $hero;
+				}
+			break; 
+			
+			default:
+				$response['error'] = true;
+				$response['message'] = 'Ninguna operacion para realizar';
+			
+		}
+		
+	}else{
+		$response['error'] = false; 
+		$response['message'] = 'Invalid Request';
+	}
+	
+	echo json_encode($response);
